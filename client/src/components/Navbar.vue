82d80557a84e6fc9cb3a0f5fa2ef3b7852386042
Linus Romland <romlandlinus@gmail.com>
2021-08-21 10:19:35 +0200

    Revert "removed console.log"
    This reverts commit a5bba36c260b88a2ca635c3803c2c767ad34554e.

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -6,23 +6,21 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-
-        <!-- Left aligned nav items -->
         <b-navbar-nav>
           <b-nav-item href="#">Link</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right v-if="data">
+          <b-nav-item href="/login">Log In</b-nav-item>
+          <b-nav-item-dropdown right>
             <!-- Using 'button-content' slot -->
             <template #button-content>
-              <em>{{ data.name }}</em>
+              <em>Username</em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="/signout">Sign Out</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
           </b-nav-item-dropdown>
-          <b-nav-item href="/login" v-else>Log In</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -31,17 +29,16 @@
 <script>
 export default {
   name: "Navbar",
-  data: () => {
+  data: function() {
     return {
-      data: "",
+      data: {},
     };
   },
   created() {
     fetch("/api/getUser")
       .then((response) => response.json())
       .then((json) => {
-        this.data = json;
-        console.log(this.data);
+        console.log(json);
       });
   },
 };
--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -7,6 +7,7 @@ module.exports = (function () {
   const login = require("../login.js");
 
   router.get("/getUser", login.checkAuthenticated, async (req, res) => {
+    console.log(await req.user);
     res.json(await req.user);
   });
 
