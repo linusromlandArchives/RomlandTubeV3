a5bba36c260b88a2ca635c3803c2c767ad34554e
Linus Romland <romlandlinus@gmail.com>
2021-08-21 10:19:20 +0200

    removed console.log

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -6,21 +6,23 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Left aligned nav items -->
         <b-navbar-nav>
           <b-nav-item href="#">Link</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="/login">Log In</b-nav-item>
-          <b-nav-item-dropdown right>
+          <b-nav-item-dropdown right v-if="data">
             <!-- Using 'button-content' slot -->
             <template #button-content>
-              <em>Username</em>
+              <em>{{ data.name }}</em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-dropdown-item href="/signout">Sign Out</b-dropdown-item>
           </b-nav-item-dropdown>
+          <b-nav-item href="/login" v-else>Log In</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -29,16 +31,17 @@
 <script>
 export default {
   name: "Navbar",
-  data: function() {
+  data: () => {
     return {
-      data: {},
+      data: "",
     };
   },
   created() {
     fetch("/api/getUser")
       .then((response) => response.json())
       .then((json) => {
-        console.log(json);
+        this.data = json;
+        console.log(this.data);
       });
   },
 };
