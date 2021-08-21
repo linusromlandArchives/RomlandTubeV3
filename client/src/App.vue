a8e3d719702be800859f1e4c6e23c9563128015d
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-08-21 20:37:38 +0200

    created site footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <Navbar />
+  <div>
+    <Navbar />
+    <Footer />
+  </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue"
 export default {
   name: "App",
   components: {
     Navbar,
+    Footer,
   },
 };
 </script>
