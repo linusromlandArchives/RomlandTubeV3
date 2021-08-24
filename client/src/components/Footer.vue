b3d083d3602193d0d32ca4eb936b841b86e86513
linusromland <48645689+linusromland@users.noreply.github.com>
2021-08-24 09:12:15 +0200

    format footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="bg-info d-flex text-light align-items-center px-3 py-3">
-		<b-container>
-			<b-row align-h="center" class="py-1">
-			<img src="assets/logo-white_transparent.png" height="45px" class="d-inline-block align-top" alt="RomlandTube Logo">
-			</b-row>
-			<b-row align-h="center" class="py-1">
-			Copyright &copy; 2021 RomlandTube
-			</b-row>
-			<b-row align-h="center" class="py-1">
-			<a href="https://github.com/linusromland/RomlandTube" class="text-light"><u>Source Code</u></a>
-			</b-row>
-		</b-container>
+    <b-container>
+      <b-row align-h="center" class="py-1">
+        <img
+          src="assets/logo-white_transparent.png"
+          height="45px"
+          class="d-inline-block align-top"
+          alt="RomlandTube Logo"
+        />
+      </b-row>
+      <b-row align-h="center" class="py-1">
+        Copyright &copy; 2021 RomlandTube
+      </b-row>
+      <b-row align-h="center" class="py-1">
+        <a href="https://github.com/linusromland/RomlandTube" class="text-light"
+          ><u>Source Code</u></a
+        >
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -19,9 +26,9 @@ export default {
   name: "Footer",
   data: () => {
     return {
-      data: "",
+      data: ""
     };
-  },
+  }
 };
 </script>
 
