9f0d22bd1916b79bbd79b062fc3e36d96dbabfb7
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2021-08-27 19:35:49 +0200

    Update Footer.vue

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,17 +1,7 @@
 <template>
-	<div class="bg-info d-flex text-light align-items-center px-3 py-3">
-		<b-container>
-			<b-row align-h="center" class="py-1">
-				<img src="assets/logo-white_transparent.png" height="45px" class="d-inline-block align-top"
-					alt="RomlandTube Logo" />
-			</b-row>
-			<b-row align-h="center" class="py-1">
-				Copyright &copy; 2021 RomlandTube
-			</b-row>
-			<b-row align-h="center" class="py-1">
-				<a href="https://github.com/linusromland/RomlandTube" class="text-light"><u>Source Code</u></a>
-			</b-row>
-		</b-container>
+	<div class="bg-info d-flex align-items-center justify-content-around p-3">
+		<a href="https://github.com/linusromland/RomlandTube/blob/master/LICENSE" class="text-light"><u>Copyright &copy; 2021 RomlandTube</u></a>
+		<a href="https://github.com/linusromland/RomlandTube" class="text-light"><u>Source Code</u></a>
 	</div>
 </template>
 
