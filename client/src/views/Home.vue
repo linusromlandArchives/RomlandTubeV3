223e595e9f432f3ffb07265f33be24541be2258d
Linus Romland <romlandlinus@gmail.com>
2021-08-27 10:05:21 +0200

    broken for loop

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,11 @@
 	<main>
 		<Navbar />
 		<h1>Home</h1>
+		<ul v-if="videos">
+			<li v-for="video in videos" :key="video.title">
+				{{ video.title }}
+			</li>
+		</ul>
 		<Footer />
 	</main>
 </template>
@@ -15,11 +20,32 @@
 			Navbar,
 			Footer,
 		},
+		data() {
+			return {
+				videos: null,
+			};
+		},
 		metaInfo() {
 			return {
 				title: "RomlandTube",
 			};
 		},
+		created() {
+			let xhr = new XMLHttpRequest();
+			//runs when return from server
+			xhr.onreadystatechange = function () {
+				if (this.readyState == 4) {
+					let json = JSON.parse(this.responseText);
+					this.videos = json;
+					console.log(this.videos)
+				}
+			};
+
+			//opens and send post request to server
+			xhr.open("GET", "/api/video/getVideos");
+			xhr.send();
+
+		}
 	};
 </script>
 
