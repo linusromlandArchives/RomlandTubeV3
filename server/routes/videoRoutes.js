4f760181bfe764578d8fd219e40f85afc66b5c87
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-08-28 16:46:35 +0200

    Fixed merge error

--- server/routes/videoRoutes.js
+++ server/routes/videoRoutes.js
@@ -4,6 +4,7 @@ module.exports = (function () {
 	const router = express.Router();
 	const fs = require("fs");
 	const resolve = require('path').resolve;
+	const sharp = require("sharp");
 
 	//Local Dependencies
 	const video = require("../video.js");
